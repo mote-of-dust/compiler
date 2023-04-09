99b77a0bd82917d2171bc59345d19d81c021b956
mote-of-dust <75862854+mote-of-dust@users.noreply.github.com>
2023-04-09 13:19:04 -0500

    pushdown stack automota
    Work on pushdown stack, as well as the logic needed to know when to pop, or keep pushing, based on the state machine made from the operator precedence table.

--- syntaxAnalyzer.cs
+++ syntaxAnalyzer.cs
@@ -2,34 +2,111 @@ using System;
 
 namespace app
 {
+
     class syntaxAnalyzer
     {
-        // //creates the operator precedence matrix based on the operator precedence table CSV input file
-        // public List<String[]> opTable()
-        // {
-        //     var csvpath = @"F:\Documents\SHSU\SHSU Spring 2023\Compiler Design\compiler_files\app\op_prec.csv";
-        //     StreamReader srOp = new StreamReader(csvpath);
-        //     var opList = new List<String[]>();
-        //     int rowsize = 0;
-        //     //int nextstate = 1;
+        public static int tCounter = 0;
 
-        //     while (!srOp.EndOfStream)
-        //     {
-        //         String[] nextline = srOp.ReadLine().Split(',');
-        //         opList.Add(nextline);
-        //         rowsize++;
-        //     }
+        // find the precedence between current and previous token, and returns the symbol found.
+        public String findSym(String prevTerm, String curToken, List<string[]> opPrecTab)
+        {
+            int colNum = 0;
+            int rowNum = 0;
+            string curTemp = "";
+            string prevTemp = "";
+            curTemp = curToken;
+            prevTemp = prevTerm;
+            if (curToken == ",")
+            {
+                curTemp = ";";
+            }
+            if (prevTerm == ",")
+            {
+                prevTemp = ";";
+            }
 
-        //     foreach (var array in opList)
-        //     {
-        //         foreach (var item in array)
-        //         {
-        //             Console.Write(item + " ");
-        //         }
-        //         Console.Write('\n');
-        //     }
-        //     return opList;
-        // }
+            for (int i = 0; i < opPrecTab[0].GetLength(0); i++)
+            {
+                if (prevTemp == opPrecTab[0][i])
+                {
+                    colNum = i;
+                }
+                else
+                {
+
+                }
+            }
+            for (int i = 0; i < opPrecTab[0].GetLength(0); i++)
+            {
+                if (curTemp == opPrecTab[0][i])
+                {
+                    rowNum = i;
+                }
+                else
+                {
+
+                }
+            }
+            Console.WriteLine(" PrevToken: " + prevTerm + " " + opPrecTab[colNum][rowNum] + " curToken: " + curToken);
+
+            return opPrecTab[colNum][rowNum];
+        }
+
+        public string popstack(List<string> pushdown, string prevTerm)
+        {
+            string tempo = "T";
+            Console.WriteLine("Pre-pop: ");
+            foreach (var item in pushdown)
+            {
+                Console.WriteLine(item);
+            }
+            int opIndx = 0;
+            List<string> popped = new List<string>();
+            for (int i = (pushdown.Count() - 1); i >= 0; i--)
+            {
+                if (prevTerm == pushdown[i])
+                {
+                    opIndx = i;
+                    break;
+                }
+            }
+            Console.WriteLine("prevTerm: " + prevTerm + " found at index " + opIndx);
+            popped.Add(pushdown[opIndx - 1]);
+            popped.Add(pushdown[opIndx]);
+            popped.Add(pushdown[opIndx + 1]);
+            pushdown.RemoveAt((opIndx + 1));
+            pushdown.RemoveAt(opIndx);
+            pushdown.RemoveAt((opIndx - 1));
+
+            //FUNCTION TO CALL SWITCHCASE AND MAKE ML CODE
+
+            Console.WriteLine("PSA after pop: ");
+            if (prevTerm != "=")
+            {
+                tCounter++;
+                pushdown.Add(tempo + tCounter.ToString());
+                foreach (var item in pushdown)
+                {
+                    Console.WriteLine(item);
+                }
+                return pushdown[(pushdown.Count) - 2];
+            }
+            else
+            {
+                foreach (var item in pushdown)
+                {
+                    Console.WriteLine(item);
+                }
+                return pushdown.Last();
+            }
+        }
+
+        // popstack() will call this function once the popped list<string> is created. This function will append a file to
+        public void mlWriter(List<string> popped)
+        {
+            // create a switch case based on item at index 1 (middle index)
+        }
+
         public void createPushdown(String[] tokenArr)
         {
             /*
@@ -45,11 +122,10 @@ namespace app
             Boolean inClass = false;
             int rowsize = 0;
             //holds value of previous found terminal, to more easily determine precedence via operator precidence table.
-            string prevTerm;
+            string prevTerm = "";
             //holds the resulting precedence from the look up table to determine if a pop needs to happen.
             string precSign;
-
-
+            int tCount = 0;
 
             // The following three lines create the operator precedence 2d matrix, by reusing the program
             // which created the original precedence table in the lexical scanner pass. reusability!modularity! p r o f e s s i o n a l g r a d e
@@ -59,6 +135,15 @@ namespace app
 
             opwords opCreator = new opwords();
             String[] opArr = opCreator.createOpArr();
+
+
+
+            /*****IMPORTANT****
+                This is the actual pushdown stack which will hold and pop code appropriately to create the machine language counterpart.
+            *****IMPORTANT****/
+            List<string> pushdown = new List<string>();
+            pushdown.Add(";");
+
 
             while (!sr.EndOfStream)
             {
@@ -89,6 +174,7 @@ namespace app
                         Console.WriteLine("Now entering program! Inserting " + tokenArr[i] + " into pushdown stack.");
                         inClass = true;
                         prevTerm = tokenArr[i];
+                        pushdown.Add(tokenArr[i]);
                     }
                     else
                     {
@@ -106,13 +192,21 @@ namespace app
                         if (array[0] == tokenArr[i])
                         {
                             Console.WriteLine("Found non-terminal " + tokenArr[i] + " within the symbol table. Pushing in stack!");
+                            pushdown.Add(tokenArr[i]);
                             pushed = true;
                         }
                         else
                         {
-                            // Console.WriteLine("array[0] = " + array[0]);
-                            // Console.WriteLine("tokenArr[i] = " + tokenArr[i]);
+                            //nothing
                         }
+                    }
+
+                    //catching edge case of variable being assigned an int with no operator functions involved ( X = 7; for example)
+                    if (pushed == false && int.TryParse(tokenArr[i], out _))
+                    {
+                        Console.WriteLine("Found non-terminal " + tokenArr[i] + " within the symbol table. Pushing in stack!");
+                        pushdown.Add(tokenArr[i]);
+                        pushed = true;
                     }
                     //now checks, if not in symbol table, if it is an operator/'terminal'. if so, a function is
                     //called to check precedence with last found terminal to determine if pop is needed.
@@ -122,24 +216,47 @@ namespace app
                         if (opArr.Contains(tokenArr[i]))
                         {
                             Console.WriteLine("Found TERMINAL " + tokenArr[i] + " within operator list. Will look up operator on precedence table...");
-                            // precSign = <function call>
-                            /* if (precSign == ">")
+                            precSign = findSym(prevTerm, tokenArr[i], opPrecTab);
+                            if (precSign == ">")
+                            {
+                                Console.WriteLine("Implementing pop logic...");
+                                // FUNCTION to pop stack and do stuff. replace earliest *possible* index with a temp (in most cases)
+                                prevTerm = popstack(pushdown, prevTerm); //update prevTerm during this step
+                                if (tokenArr[i] != ";")
                                 {
-                                    <function to pop correct lines, create machine code via switch, 
-                                    and create T[x] temp and place in stack if needed.>                                
+                                    pushdown.Add(tokenArr[i]);
                                 }
 
-                                more logic to be added here I think...
-                            */
-                            //update prevTerm at the end to continue the cycle of easily recalling previous terminal.
-                            prevTerm = tokenArr[i];
+                            }
+                            else if (precSign == "@")
+                            {
+                                Console.WriteLine("special code to be implemented for this pop.");
+                                // special case logic/pop for a declaration.
+                                pushdown.Add(tokenArr[i]);
+                                //update prevTerm at the end to continue the cycle of easily recalling previous terminal.
+                                prevTerm = tokenArr[i];
+                            }
+                            else
+                            {
+                                Console.WriteLine("operator yields; Placing in stack...");
+                                pushdown.Add(tokenArr[i]);
+                                //update prevTerm at the end to continue the cycle of easily recalling previous terminal.
+                                prevTerm = tokenArr[i];
+                            }
+
+
+
 
                         }
 
                     }
                     //Console.WriteLine("Pushing " + tokenArr[i] + " into the stack!");
                 }
-
+            }
+            Console.WriteLine("Possible end??");
+            foreach (var item in pushdown)
+            {
+                Console.WriteLine(item);
             }
         }
     }
